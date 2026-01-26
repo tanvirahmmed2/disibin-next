a12949293d33c78deba75e480b5d7f19abfd7b99
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-27 00:38:30 +0600

    bg

--- src/component/pages/Intro.jsx
+++ src/component/pages/Intro.jsx
@@ -1,37 +1,31 @@
 'use client'
+import Image from 'next/image'
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 
 const Intro = () => {
   return (
-    <div className='w-full min-h-150 flex flex-col md:flex-row items-center justify-center gap-4 p-4'>
+    <div  className='w-full h-150 flex flex-col items-center justify-center gap-4 p-4 relative text-white'>
+      <Image src={'/bg.jpeg'} alt='bg' width={1000} height={1000} className='w-full h-150 absolute -z-10 opacity-65'/>
 
 
-      <div className='w-full flex flex-col gap-3'>
-        <p className='text-2xl font-semibold'>A Leading Software Development Company Worldwide</p>
-        <p className='text-8xl font-semibold text-sky-600'>
-          <Typewriter
-            words={[
-              "Disibin",
-              'Ultimate Solution',
-              "100X Business Growth"
-            ]}
-            loop={0}
-            cursor
-            cursorStyle="|"
-            typeSpeed={80}
-            deleteSpeed={50}
-            delaySpeed={1500}
-          />
-        </p>
-        <p className='text-2xl'>Delivering innovative, scalable, and high-performance digital solutions for businesses across the globe.</p>
-      </div>
-
-
-
-      <div className='w-full flex flex-col gap-3'>
-
-      </div>
+      <p className='text-2xl font-semibold'>A Leading Software Development Company Worldwide</p>
+      <p className='text-8xl font-semibold text-sky-600'>
+        <Typewriter
+          words={[
+            "Disibin",
+            'Ultimate Solution',
+            "100X Business Growth"
+          ]}
+          loop={0}
+          cursor
+          cursorStyle="|"
+          typeSpeed={80}
+          deleteSpeed={50}
+          delaySpeed={1500}
+        />
+      </p>
+      <p className='text-2xl'>Delivering innovative, scalable, and high-performance digital solutions for businesses across the globe.</p>
 
     </div>
   )

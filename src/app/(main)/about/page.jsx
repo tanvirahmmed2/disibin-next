54fb0a53343f98caf079d7847263b343792f0780
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-28 03:31:52 +0600

    colour

--- src/app/(main)/about/page.jsx
+++ src/app/(main)/about/page.jsx
@@ -27,13 +27,13 @@ const AboutPage = () => {
 
         </div>
 
-        <div className='w-full text-center p-2 shadow  flex flex-row items-center justify-center gap-2 border border-sky-500 rounded-xl border-opacity-20'>
+        <div className='w-full text-center p-2 shadow  flex flex-row items-center justify-center gap-2 border border-teal-500 rounded-xl border-opacity-20'>
           <motion.div initial={{ scale: 0.9, opacity: 0 }} whileInView={{ scale: 1, opacity: 1 }} transition={{ duration: 0.5 }} className='w-full flex flex-col items-center justify-center h-37.5 bg-white/5 rounded-lg text-xl font-semibold'>
             <p>20+</p>
             <p>Projects shipped</p>
           </motion.div>
           <motion.div initial={{ scale: 0.9, opacity: 0 }} whileInView={{ scale: 1, opacity: 1 }} transition={{ duration: 0.5 }} className='w-full flex flex-col items-center justify-center h-37.5 bg-white/5 rounded-lg text-xl font-semibold'>
-            <p className='flex items-center gap-1 text-sky-500'>5 <FaRegStar /></p>
+            <p className='flex items-center gap-1 text-teal-500'>5 <FaRegStar /></p>
             <p>Customers rated</p>
           </motion.div>
           <motion.div initial={{ scale: 0.9, opacity: 0 }} whileInView={{ scale: 1, opacity: 1 }} transition={{ duration: 0.5 }} className='w-full flex flex-col items-center justify-center h-37.5 bg-white/5 rounded-lg text-xl font-semibold'>
@@ -44,7 +44,7 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <div className='w-full p-4 bg-sky-50 text-center py-5 flex flex-col gap-4'>
+      <div className='w-full p-4 bg-teal-50 text-center py-5 flex flex-col gap-4'>
         <p className='text-center w-full font-semibold'>DisiBin, is a dynamic and forward-thinking digital agency committed to transforming ideas into impactful digital experiences. We provide end-to-end services that blend strategy, creativity, and technology to help businesses thrive in the modern digital landscape.</p>
         <p className='text-center w-full font-semibold'>Our mission is to empower businesses—big or small—to succeed onne. We believe in honest communication, long-term partnerships, and devering measurable results. Whether you’re launching a new product, redesigning your website, or building a brand from scratch, our team is here to make it happen.</p>
 
@@ -52,7 +52,7 @@ const AboutPage = () => {
       </div>
       <div className="w-full p-4 flex flex-col lg:flex-row items-center justify-center gap-8">
         <motion.div initial={{ opacity: 0, x: -40 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} className="w-full flex flex-col gap-8 items-center justify-center">
-          <div className="w-full h-30 p-4 flex flex-row items-center justify-start gap-2 shadow  border border-sky-500 rounded-xl border-opacity-20">
+          <div className="w-full h-30 p-4 flex flex-row items-center justify-start gap-2 shadow  border border-teal-500 rounded-xl border-opacity-20">
             <p className="text-2xl px-3 py-1 rounded-full bg-white/10">1</p>
             <div>
               <h1 className="text-2xl font-semibold">Discover</h1>
@@ -62,14 +62,14 @@ const AboutPage = () => {
               </p>
             </div>
           </div>
-          <div className="w-full h-30 p-4 flex flex-row items-center justify-start gap-2 shadow  border border-sky-500 rounded-xl border-opacity-20">
+          <div className="w-full h-30 p-4 flex flex-row items-center justify-start gap-2 shadow  border border-teal-500 rounded-xl border-opacity-20">
             <p className="text-2xl px-3 py-1 rounded-full bg-white/10">2</p>
             <div>
               <h1 className="text-2xl font-semibold">Design</h1>
               <p>Homepage first, then inner pages. Feedback at every step</p>
             </div>
           </div>
-          <div className="w-full h-30 p-4 flex flex-row items-center justify-start gap-2 shadow  border border-sky-500 rounded-xl border-opacity-20">
+          <div className="w-full h-30 p-4 flex flex-row items-center justify-start gap-2 shadow  border border-teal-500 rounded-xl border-opacity-20">
             <p className="text-2xl px-3 py-1 rounded-full bg-white/10">3</p>
             <div>
               <h1 className="text-2xl font-semibold">Build & Launch</h1>
@@ -80,7 +80,7 @@ const AboutPage = () => {
 
         <motion.div initial={{ opacity: 0, x: 40 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }} className="w-full h-auto p-2 flex-col flex items-center justify-center  gap-8">
           <h1 className="text-2xl">FAQs</h1>
-          <div className="w-full p-4 shadow  flex flex-col gap-2 border border-sky-500 rounded-xl border-opacity-20">
+          <div className="w-full p-4 shadow  flex flex-col gap-2 border border-teal-500 rounded-xl border-opacity-20">
             <div className="w-full flex flex-row items-center justify-between">
               <p>Do you write copy?</p>
               <p onClick={handlefirstfaq} className={`cursor-pointer`}>
@@ -91,7 +91,7 @@ const AboutPage = () => {
               Yes, we can help with messaging, headlines, and structure.
             </p>
           </div>
-          <div className="w-full p-4 shadow  flex flex-col gap-2 border border-sky-500 rounded-xl border-opacity-20">
+          <div className="w-full p-4 shadow  flex flex-col gap-2 border border-teal-500 rounded-xl border-opacity-20">
             <div className="w-full flex flex-row items-center justify-between">
               <p>What tools do you use?</p>
               <p onClick={handlesecondfaq} className={`cursor-pointer `}>
@@ -102,7 +102,7 @@ const AboutPage = () => {
               Simple, dependable tools to keep things fast and stable.
             </p>
           </div>
-          <div className="w-full p-4 shadow  flex flex-col gap-2 border border-sky-500 rounded-xl border-opacity-20">
+          <div className="w-full p-4 shadow  flex flex-col gap-2 border border-teal-500 rounded-xl border-opacity-20">
             <div className="w-full flex flex-row items-center justify-between">
               <p>How fast can we launch?</p>
               <p onClick={handlethirdfaq} className={`cursor-pointer `}>

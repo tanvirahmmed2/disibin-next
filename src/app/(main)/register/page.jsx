44ac82d7dedbbc5475d40b6d76159182c7964c6c
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-28 02:46:15 +0600

    account

--- src/app/(main)/register/page.jsx
+++ src/app/(main)/register/page.jsx
@@ -1,9 +1,19 @@
+import RegisterForm from '@/component/forms/RegisterForm'
 import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>
-      
+    <div className='w-full flex items-center justify-center h-screen p-4'>
+      <div className='w-full sm:w-3/4 md:w-1/2 h-auto p-4 border-0 sm:border-2 rounded-xl border-black/10 shadow flex flex-col md:flex-row items-center justify-center '>
+        <RegisterForm />
+        <div className='flex-1 flex flex-col gap-1 items-center justify-center'>
+          <p>Welcome to</p>
+          <h1 className='text-3xl font-semibold'>Disibin</h1>
+          <p>Your best assistant</p>
+
+        </div>
+
+      </div>
     </div>
   )
 }
---
+++ src/component/forms/RegisterForm.jsx
@@ -0,0 +1,36 @@
+'use client'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import React, { useState } from 'react'
+
+const RegisterForm = () => {
+    const [formData, setFormData]= useState({
+      name:'',
+        email:'',
+        password:''
+    })
+    const handleChange=(e)=>{
+        const {name, value}= e.target
+        setFormData((prev)=>({...prev,[name]:value}))
+    }
+  return (
+    <motion.form initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1}} className='flex-1 flex-col flex '>
+        <div className='w-full flex flex-col gap-1'>
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' name='name' required onChange={handleChange} value={formData.name} className='w-full px-3 p-1 border rounded-sm outline-none'/>
+        </div>
+        <div className='w-full flex flex-col gap-1'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' required onChange={handleChange} value={formData.email} className='w-full px-3 p-1 border rounded-sm outline-none'/>
+        </div>
+        <div className='w-full flex flex-col gap-1'>
+            <label htmlFor="password" >Password</label>
+            <input type="password" name='password' id='password' required onChange={handleChange} value={formData.password} className='w-full px-3 p-1 border rounded-sm outline-none'/>
+        </div>
+        <Link className='text-right' href={'/login'}>already user?</Link>
+        <button type='submit' className='px-4 bg-sky-900 text-white p-1 cursor-pointer hover:bg-sky-700'>Register</button>
+    </motion.form>
+  )
+}
+
+export default RegisterForm

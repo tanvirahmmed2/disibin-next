387283ee95108204d433afa8c3568dc643f5bfa0
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-30 21:28:04 +0600

    restart

--- src/component/pages/HappyCustomers.jsx
+++ src/component/pages/HappyCustomers.jsx
@@ -1,10 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HappyCustomers = () => {
   return (
     <div className='w-full p-4 flex flex-col items-center justify-center gap-6 py-8'>
       <h1 className='text-2xl font-semibold'>Our Happy Customers</h1>
-      
+      <div>
+
+      </div>
+      <Link href={'/reviews'} className='px-4 p-1 bg-teal-700 text-white rounded-full'>More . . .</Link>
     </div>
   )
 }

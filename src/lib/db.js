5c8cf85bb57f6d82daa93571ec04a63bccf74661
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-31 02:24:46 +0600

    secure urls

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
+import { PG_DATABASE, PG_HOST, PG_PASSWORD, PG_PORT, PG_USER } from "./secret";
 
 export const pool= new Pool({
-  user:process.env.PG_USER,
-  password:process.env.PG_PASSWORD,
-  host:process.env.PG_HOST,
-  port:process.env.PG_PORT,
-  database:process.env.PG_DATABASE,
+  user:PG_USER,
+  password:PG_PASSWORD,
+  host:PG_HOST,
+  port:PG_PORT,
+  database:PG_DATABASE,
   ssl: {
     rejectUnauthorized: false, 
   },
---
+++ src/lib/secret.js
@@ -0,0 +1,8 @@
+export const BASE_URL=process.env.BASE_URL
+export const NODE_ENV=process.env.NODE_ENV
+
+export const PG_USER=process.env.PG_USER
+export const PG_PASSWORD=process.env.PG_PASSWORD
+export const PG_HOST=process.env.PG_HOST
+export const PG_PORT=process.env.PG_POST
+export const PG_DATABASE=process.env.PG_DATABASE
